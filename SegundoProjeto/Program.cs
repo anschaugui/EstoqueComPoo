6589d68209e5b718d50fc353b241492b2de4dd54
anschaugui <anschau.gui@gmail.com>
2025-02-06 11:09:33 -0300

    projeto de estoque utilizando c# com Poo

---
+++ SegundoProjeto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SegundoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto1 = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.WriteLine("nome: ");
+            produto1.Nome = Console.ReadLine();
+            Console.WriteLine("preco: ");
+            produto1.Preco = double.Parse(Console.ReadLine(), System.Globalization.NumberStyles.Currency);
+            Console.WriteLine("Quantidade em estoque: ");
+            produto1.Quantidade = int.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
